61f6b6d3ba6f875c1ec797ac58335b8874e28ec0
Simon <simonhammes@users.noreply.github.com>
2023-12-18 23:08:13 +0100

    Update to 0.1.2

--- HomebrewFormula/mcfly-fzf.rb
+++ HomebrewFormula/mcfly-fzf.rb
@@ -5,8 +5,8 @@
 class McflyFzf < Formula
   desc "Integrate McFly with fzf"
   homepage "https://github.com/bnprks/mcfly-fzf"
-  url "https://github.com/bnprks/mcfly-fzf/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "6074ed3d84d90f43ad1def5be1313275d4220c14f70de54c2bf706ae16b73a9f"
+  url "https://github.com/bnprks/mcfly-fzf/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "353b35769cef7a1c67e1b310810e306863e4337f8bb8144d002a2e15ad3906f2"
   license "MIT"
 
   depends_on "rust" => :build
