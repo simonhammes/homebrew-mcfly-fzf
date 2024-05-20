94ca8313051f2841344431a6533f1a975faad203
simonhammes <simonhammes@users.noreply.github.com>
2024-05-20 10:38:55 +0200

    Bump version to 0.1.3

--- HomebrewFormula/mcfly-fzf.rb
+++ HomebrewFormula/mcfly-fzf.rb
@@ -5,12 +5,12 @@
 class McflyFzf < Formula
   desc "Integrate McFly with fzf"
   homepage "https://github.com/bnprks/mcfly-fzf"
-  url "https://github.com/bnprks/mcfly-fzf/archive/refs/tags/0.1.2.tar.gz"
+  url "https://github.com/bnprks/mcfly-fzf/archive/refs/tags/0.1.3.tar.gz"
   sha256 "353b35769cef7a1c67e1b310810e306863e4337f8bb8144d002a2e15ad3906f2"
   license "MIT"
 
   bottle do
-    root_url "https://github.com/simonhammes/homebrew-mcfly-fzf/releases/download/mcfly-fzf-0.1.2"
+    root_url "https://github.com/simonhammes/homebrew-mcfly-fzf/releases/download/mcfly-fzf-0.1.3"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "799d2b2352c8300a26c8cced24162c79c57b126ae6ae24c5636a38900392813c"
   end
 
